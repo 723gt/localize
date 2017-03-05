b484f3f007420fc40573bf16e4063b56d707cc34
723 <rb.natsumi@gmail.com>
2017-03-05 11:04:59 +0900

    🔧 loginのコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'timeline' => 'timeline#index'
 
-
   get 'signup' => 'signup#new'
   post 'signup' => 'signup#create'
+  
+  get 'login' => 'login#new'
+  post 'login' => 'login#create'
 end
