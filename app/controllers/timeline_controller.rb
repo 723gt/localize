9613b0ea3da38159932c280288bc143595afb48c
723 <rb.natsumi@gmail.com>
2017-03-14 08:13:40 +0900

    🔧 TimeLine表示を作成

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,5 +3,11 @@ class TimelineController < ApplicationController
     if !session[:user_name]
       redirect_to(:controller => 'login',:action => 'new')
     end
+    follower = Follow.where(user_id: session[:user_id])
+    follower_id = Array.new
+    follower.each_with_index do |f,i|
+      follower_id[i] = f.follow_id
+    end
+    @time_line = Post.where(user_id: follower_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts
-  has_many :follow
+#  has_many :posts,foreign_key: "user_id"
+  #has_many :follow, foreign_key: "user_id"
 end
