335e30e29c8ef57765b1ec0dc86a250e02ec3605
723 <rb.natsumi@gmail.com>
2017-03-15 13:26:34 +0900

    🔧 Postを作成

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -8,6 +8,26 @@ class TimelineController < ApplicationController
     follower.each_with_index do |f,i|
       follower_id[i] = f.follow_id
     end
-    @time_line = Post.where(user_id: follower_id)
+    @time_line = Post.where(user_id: follower_id).order("updated_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    puts params[:post][:content]
+    posts = Post.new(post_params)
+    if posts.save
+      session[:post_err] = nil
+    else
+      session[:post_err] = "投稿に失敗しました"
+    end
+    redirect_to(:action => 'index')
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id,:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'localize#index'
   get 'timeline' => 'timeline#index'
+  get 'timeline/new' => 'timeline#new',as: :timeline_new
+  post 'timeline/create' => 'timeline#create',as: :timeline_create
 
   get 'signup' => 'signup#new'
   post 'signup' => 'signup#create'
